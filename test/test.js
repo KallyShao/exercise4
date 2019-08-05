c000587581a2783217b1bdfe90e90f8fecbe56eb
Kally <188963758@qq.com>
2019-08-05 22:37:07 +0800

    测试通过，但有疑问

--- test/test.js
+++ test/test.js
@@ -4,7 +4,8 @@ describe('this', function () {
       say: function () {
         setTimeout(() => {
           // this 是什么？想想为什么？
-          this.should.equal(null)
+          // 这里this是指调用say()函数的obj,箭头函数不会对this产生影响
+          this.should.equal(obj)
           done()
         }, 0)
       }
@@ -15,7 +16,8 @@ describe('this', function () {
   it('global', function () {
     function test() {
       // this 是什么？想想为什么？
-      this.should.equal(null)
+      // 这里this指向全局对象；如果是在浏览器中执行，指向window对象
+      this.should.equal(global);
     }
     test()
   })
@@ -26,23 +28,25 @@ describe('this', function () {
         say: function () {
           function _say() {
             // this 是什么？想想为什么？
-            this.should.equal(null)
+            // 这里不太懂
+            this.should.equal(global)
           }
           return _say.bind(obj)
         }()
-      }
+      };
       obj.say()
     })
 
     it('bind normal', function () {
-      var obj = {}
+      var obj = {};
       obj.say = function () {
         function _say() {
           // this 是什么？想想为什么？
-          this.should.equal(null)
+          // 这里不太懂
+          this.should.equal(obj)
         }
         return _say.bind(obj)
-      }()
+      }();
       obj.say()
     })
   })
